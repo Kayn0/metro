4c88dd070b7de137bcbaa56b5299685957d46b4e
kane.maslen <kanemaslen@gmail.com>
2021-05-04 18:18:58 +1000

    Add placeholder text and request btn to Services pages

--- src/views/Services/Financing.vue
+++ src/views/Services/Financing.vue
@@ -7,14 +7,49 @@
       <div class="image-container"></div>
     </div>
     <div class="content">
-      <h3>This is the Sevices > Financing page</h3>
+      <h3>Fixed Fees, no hidden costs.</h3>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+        incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+        exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+      </p>
+
+      <p>
+        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+        fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+        culpa qui officia deserunt mollit anim id est laborum.
+      </p>
+      <button @click="isShowingQuoteRequest = !isShowingQuoteRequest" class="quote-btn">
+        Request Quote
+      </button>
+      <div v-if="isShowingQuoteRequest">
+        <RequestQuote
+          :parentData="isShowingQuoteRequest"
+          v-on:emitCloseModal="closeModal"
+        />
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import RequestQuote from "@/components/RequestQuote/RequestQuote.vue";
+
 export default {
   name: "financing",
+  data() {
+    return {
+      isShowingQuoteRequest: false,
+    };
+  },
+  methods: {
+    closeModal() {
+      this.isShowingQuoteRequest = false;
+    },
+  },
+  components: {
+    RequestQuote,
+  },
   created() {
     document.title = "Financing Property";
   },
--- src/views/Services/Selling.vue
+++ src/views/Services/Selling.vue
@@ -10,13 +10,50 @@
     </div>
     <div class="content">
       <h3>First time selling?</h3>
+      <p>
+        If you have decided to sell your property the first thing you should do is consult
+        your solicitor or conveyancer. The law requires a contract to be available for
+        inspection by prospective purchasers, so doing this before your property is listed
+        or advertised for sale will save you time.
+      </p>
+      <p>
+        Some sellers (vendors) like to act for themselves but you still need to have a
+        contract prepared before proceeding. Assuming you are using an agent, Metro
+        Conveyancing can prepare a contract in accordance with your instructions and the
+        legal requirements for forwarding to your nominated agent.
+      </p>
+
+      <button @click="isShowingQuoteRequest = !isShowingQuoteRequest" class="quote-btn">
+        Request Quote
+      </button>
+      <div v-if="isShowingQuoteRequest">
+        <RequestQuote
+          :parentData="isShowingQuoteRequest"
+          v-on:emitCloseModal="closeModal"
+        />
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import RequestQuote from "@/components/RequestQuote/RequestQuote.vue";
+
 export default {
   name: "selling",
+  data() {
+    return {
+      isShowingQuoteRequest: false,
+    };
+  },
+  methods: {
+    closeModal() {
+      this.isShowingQuoteRequest = false;
+    },
+  },
+  components: {
+    RequestQuote,
+  },
   created() {
     document.title = "Selling Property";
   },
